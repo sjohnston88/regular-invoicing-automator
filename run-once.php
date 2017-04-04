dba523842dbd70d1b26ea1888889566734e27190
Sam Johnston <samjohnston@Sams-MacBook-Air.local>
2017-04-04 13:21:41 -0400

    First Commit

---
+++ run-once.php
@@ -0,0 +1,125 @@
+<?php
+
+// Initial Setup 
+
+require './dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+
+require './PHPMailer/class.phpmailer.php';
+
+date_default_timezone_set('Europe/London');
+$services = '';
+$rollDate = date("d/m/Y"); 
+$saveDate = date("dmY");
+$dueDate = date("d/m/Y", strtotime('next month'));
+
+// Get config JSON data
+
+$json = file_get_contents('config.json');
+$config = json_decode($json, true);
+
+extract($config, EXTR_PREFIX_SAME, "config");
+
+// Loop through clients
+
+$clients = json_decode(file_get_contents('clients.json'));
+
+foreach ($clients->clients as $client){
+    
+    // Generate services rendered
+    foreach($client->services as $service){
+        $services .= '<tr class="item"><td>'.$service->service.'</td>';
+        $services .= '<td>&pound;'.$service->price.'</td></tr>';
+    }
+    
+    // Generate PDF Invoice
+    $dompdf = new Dompdf();
+    $dompdf->set_option('isHtml5ParserEnabled', true);
+    
+    include './template.php';
+    
+    $dompdf->loadHtml($invoiceHTML);
+    $dompdf->render();
+    $output = $dompdf->output();
+    
+    // Save Invoice
+    $invoiceCompany = str_replace(' ', '-', $client->company);
+    $filename = 'Invoice-'.$client->companyShortname.$client->invoiceNumber.'-'.$invoiceCompany.'-'.$saveDate.'.pdf';
+    file_put_contents('./invoices/'.$filename, $output);
+    
+    // Enable to assist debugging
+    // echo $filename . ' has been written.<br />';
+    
+    // Update clients.json
+    $client->invoiceNumber = $client->invoiceNumber + 1;
+    $updateJson = json_encode($clients, JSON_PRETTY_PRINT);
+    file_put_contents('clients.json', $updateJson);
+    
+    // Enable to assist debugging
+    // echo $filename . ' invoice number has been increased by 1 ('.$client->invoiceNumber.')<br />';
+    
+    // Create new mailer
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();
+    
+    // Enable for debugging
+    // $mail->SMTPDebug = 2; 
+    
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Host = $mailHost;
+    $mail->Port = $mailPort;
+    $mail->Username = $mailUsername;
+    $mail->Password = $mailPassword;
+
+    $mail->From = $email;
+    $mail->FromName = $name;
+    $mail->addAddress($client->email);
+    $mail->addReplyTo($email, $name);
+    $mail->addBCC($email);
+
+    $mail->addAttachment('./invoices/'.$filename);
+
+    $mail->Subject = $mailSubject;
+
+    $mail->Body  = "Dear, " .$client->name.",\r\n\r\n";
+    $mail->Body .= $emailMessage;
+    $mail->Body .= "\r\n\r\nMany Thanks,\r\n";
+    $mail->Body .= $name;
+    
+    // Send the mailer
+    if(!$mail->send()) {
+        // Enable to assist debugging
+        // echo 'Message could not be sent.';
+        // echo 'Mailer Error: ' . $mail->ErrorInfo;
+  		
+        // Send Error Email to Admin
+        $to      = $email;
+		$subject = 'Error: New Invoice Failed to Send';
+		$message = 'An invoice due for payment by ' .$dueDate. 'has failed to send to ' . $client->name;
+		$headers = 'From:' . $email . "\r\n" .
+                   'Reply-To:' . $email . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+						
+		mail($to, $subject, $message, $headers);
+        
+    } else {
+        
+        // Enable to assist debugging
+        echo $filename.' has been sent to the client<br /><br />';
+        
+        // Send Success Email to Admin
+        $to      = $email;
+		$subject = 'New Invoice Created';
+		$message = 'An invoice due for payment by ' .$dueDate. 'has sucessfully been sent to.' . $client->name;
+		$headers = 'From:' . $email . "\r\n" .
+                   'Reply-To:' . $email . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+    }
+    
+    // Unset services ready for next run
+    $services = '';
+}
+
+?>
---
+++ template.php
@@ -0,0 +1,52 @@
+<?php
+
+$invoiceHTML = <<<HTML
+    <!DOCTYPE html><html><head><title>Invoice</title>
+    <style type="text/css"> .invoice-box { max-width: 800px; margin: auto; margin: 30px; padding: 30px; border: 1px solid #eee; font-size: 16px; line-height: 24px; font-family: sans-serif; color: #555; } .invoice-box table { width: 100%; text-align: left; } .invoice-box table td { padding: 10px; vertical-align: top; } .invoice-box table tr td:nth-child(2) { text-align: right; } .invoice-box table tr.top table td { padding-bottom: 20px; } .invoice-box table tr.top table td.title { font-size: 45px; line-height: 45px; color: #333; } .invoice-box table tr.information table td { padding-bottom: 40px; } .invoice-box table tr.heading td { background: #eee; border-bottom: 1px solid #ddd; padding: 10px 10px; font-weight: bold; } .invoice-box table tr.details td { padding-bottom: 20px; } .invoice-box table tr.item td { border-bottom: 1px solid #eee; } .invoice-box table tr.item.last td { border-bottom: none; } .invoice-box table tr.total td:nth-child(2) { font-weight: bold; } </style></head><body> 
+    <div class="invoice-box"> 
+    <table cellpadding="0" cellspacing="0"> 
+    <tr class="top"> 
+    <td colspan="2"> 
+    <table cellpadding="0" cellspacing="0">
+    <tr>
+    <td class="title">$title</td>
+    <td>Invoice #: $client->companyShortname-$client->invoiceNumber<br>Date: $rollDate<br>Due: $dueDate<br>Method: $paymentMethod</td>
+    </tr>
+    </table>
+    </td>
+    </tr>
+    <tr class="information">
+    <td colspan="2">
+    <table cellpadding="0" cellspacing="0">
+    <tr>
+    <td>$name<br><b>T:</b> $phone<br><b>E:</b> $email</td>
+    <td>$client->name<br>$client->company<br>$client->address</td>
+    </tr>
+    </table>
+    </td>
+    </tr>
+    <tr class="heading">
+    <td>Description</td>
+    <td>Amount</td></tr>
+    $services
+    <tr class="total">
+    <td></td>
+    <td>Total: &pound;$client->total</td>
+    </tr>
+    <tr class="information">
+    <td colspan="2">
+    <table cellpadding="0" cellspacing="0">
+    <tr>
+    <td><b>Payable To</b><br>$name<br>$bankName<br>$sortCode<br>$accountNumber</td>
+    <td><b>Address</b><br>$address1<br>$address2<br>$country</td>
+    </tr>
+    </table>
+    </td>
+    </tr>
+    </table>
+    </div>
+    </body>
+    </html>
+HTML;
+
+?>
